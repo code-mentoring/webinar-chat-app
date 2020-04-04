6dc4230cb96acb6da2352c28ade8ddb2f1a57eb2
Tristan Matthias <hello@tristanmatthias.com>
2020-04-04 13:28:48 -0400

    feat(api): added token to all requests

--- packages/client/src/lib/API.ts
+++ packages/client/src/lib/API.ts
@@ -4,54 +4,64 @@ class API {
   prefix = 'http://localhost:9999';
 
   async login(email: string, password: string) {
-    try {
-      const res = await axios.post(`${this.prefix}/auth/login`, {
-        email, password
-      });
-      return res.data;
-    } catch (e) {
-      return null;
-    }
+    return this.request('post', '/auth/login', {
+      email, password
+    });
   }
 
   async getConversations() {
-    try {
-      const res = await axios.get(`${this.prefix}/conversations`);
-      return res.data;
-    } catch (e) {
-      return null;
-    }
+    return this.request('get', '/conversations');
   }
 
   async getConversation(id: string) {
-    try {
-      const res = await axios.get(`${this.prefix}/conversations/${id}`);
-      return res.data;
-    } catch (e) {
-      return null;
-    }
+    return this.request('get', `/conversations/${id}`);
   }
 
   async createConversation(name: string) {
-    const res = await axios.post(`${this.prefix}/conversations`, { name });
-    return res.data;
+    return this.request('post', '/conversations', { name });
   }
 
   async getMessages(id: string) {
-    const res = await axios.get(`${this.prefix}/conversations/${id}/messages`);
-    return res.data;
+    return this.request('get', `/conversations/${id}/messages`);
   }
 
   async createMessage(
     conversationId: string,
     content: string
   ) {
-    const res = await axios.post(`${this.prefix}/messages`, {
+    return this.request('post', '/messages', {
       userId: 'd6147162-7377-4236-ab1d-a641f577c3b7',
       content,
       conversationId
     });
-    return res.data;
+  }
+
+
+  private async request(
+    type: 'get' | 'post',
+    url: string,
+    data?: object
+  ) {
+    const headers = {
+      authorization: `Bearer ${localStorage.getItem('token')}`
+    };
+
+    try {
+      let res: any;
+      if (type === 'get') {
+        res = await axios.get(`${this.prefix}${url}`, {
+          headers
+        });
+      }
+      if (type === 'post') {
+        res = await axios.post(`${this.prefix}${url}`, data, {
+          headers
+        });
+      }
+      return res!.data;
+    } catch (e) {
+      return null;
+    }
   }
 }
 export const api = new API();
